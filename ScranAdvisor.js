462d519cb7e9ac9f848bd319587c6491b8599762
Thomas Loughran <tloughran3@gmail.com>
2023-11-22 14:46:54 +0000

    passes first 4 tests

--- ScranAdvisor.js
+++ ScranAdvisor.js
@@ -2,6 +2,18 @@ const ScranAdvisor = function(restaurants){
     this.restaurants = restaurants;
 }
 
-// Add methods to prototype
+ScranAdvisor.prototype.numberOfRestaurants = function(){
+    return this.restaurants.length;
+}
+
+ScranAdvisor.prototype.findByName = function(restaurantName){
+    return this.restaurants.find((restaurant) => {
+        return restaurant.name === restaurantName;
+    })
+}
+
+ScranAdvisor.prototype.findAllNames = function(){
+    return this.restaurants.map((restaurant) => restaurant.name);
+}
 
 module.exports = ScranAdvisor;
